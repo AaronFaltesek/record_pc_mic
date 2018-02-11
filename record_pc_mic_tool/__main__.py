e14b20b0b5e98c26526c6d50b848935fefebebd5
gitatx <aaron.faltesek@gmail.com>
2018-02-11 17:58:47 -0600

    AKF: add config

--- record_pc_mic_tool/__main__.py
+++ record_pc_mic_tool/__main__.py
@@ -1,7 +1,13 @@
 
+def set_config():
+    config={"record_prgm_path":"/c/example"}
+    return config
+
 def main():
     print("record_pc_mic_tool main!")
-    print("Develop branch test")
+    print("Set config")
+    config = set_config()
+    print(config.__str__())
 
 if __name__ == "__main__":
     main()
