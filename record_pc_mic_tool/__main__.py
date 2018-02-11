7705b05367f8bf2548ac47d01d7c3e6de64f8218
AaronFaltesek <aaron.faltesek@gmail.com>
2018-02-11 16:05:34 -0600

    AKF: init project

---
+++ record_pc_mic_tool/__main__.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("record_pc_mic_tool main!")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='record_pc_mic_tool',
+    version='1.0',
+    descript='Tool to record audio through my pc mic on windows 10',
+    py_modules=['record_pc_mic_tool'],
+    install_requires=[''] #external packages as dependencies
+)
